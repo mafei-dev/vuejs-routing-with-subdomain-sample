a303327e6d7d1708bf854225a3a9bf4baf668d03
mafei <mafei.dev@gmail.com>
2020-02-02 05:29:37 +0530

    change dist

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     outputDir: process.env.NODE_ENV === 'production'
-        ? "/var/www/html/front/dist"
-        : '/',
+        // ? "/var/www/html/admin/dist" /*if you want you can change*/
+        ? "/dist"
+        : '/dist',
     publicPath: "./"
 }
