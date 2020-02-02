f3bdde5944ee3e4059aaa0638201189d73206ab6
mafei <mafei.dev@gmail.com>
2020-02-02 19:45:18 +0530

    dist

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     outputDir: process.env.NODE_ENV === 'production'
         // ? "/var/www/html/admin/dist" /*if you want you can change*/
-        ? __dirname+"/dist"
+        ? __dirname + "/dist"
         : '/dist',
-    publicPath: "../admin",
+    publicPath: "./",
 
 }
